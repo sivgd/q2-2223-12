a0760000f03eb871510d80041078397ac1cefe5e
JohnF <boxtrotterror@gmail.com>
2022-12-12 02:07:42 -0800

    Made the Lilypad Sniper

---
+++ Q2-2223-T12/Assets/John/Scripts/Weapons/LilypadSniper.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LilypadSniper : MonoBehaviour
+{
+    [Header("External References")]
+    public GameObject lilypadPrefab;
+    public Transform instPos;
+
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot(); 
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(lilypadPrefab, instPos.position, instPos.rotation); 
+    }
+}
