a7d6a354e1caf86350ddfb668855d69fecab45ec
MUKILTEO\1710136 <1710136@mukilteo.wednet.edu>
2022-12-13 10:25:03 -0800

    stuff

---
+++ Q2-2223-T12/Assets/Shreyas/AIScripts/ExplodeImmediatly.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeImmediatly : MonoBehaviour
+{
+    EnemyHealth health;
+    EnemyAI enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
