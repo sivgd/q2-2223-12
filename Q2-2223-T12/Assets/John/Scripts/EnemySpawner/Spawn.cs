a37e6d8bfee0c6abc9f558ab65384c4836ca0087
VGD_2022x23_John_Fotheringham <1713323@mukilteo.wednet.edu>
2022-12-14 10:20:49 -0800

    Fixed enemy spawning issues

--- Q2-2223-T12/Assets/John/Scripts/EnemySpawner/Spawn.cs
+++ Q2-2223-T12/Assets/John/Scripts/EnemySpawner/Spawn.cs
@@ -14,32 +14,55 @@ public class Spawn : MonoBehaviour
 
     private int currentEnemy = 0;
     private bool canSpawn = false;
+    private GameObject[] instObj;
     private bool enemyExsists;
+
+
+    private void Start()
+    {
+        instObj = new GameObject[enemyQueue.Length]; 
+    }
     /// <summary>
     /// Spawns a new enemy if there aren't enemies in the scene
     /// iterates on what enemy can be spawned after it spawns an enemy, and said enemy is destroyed
     /// <returns></returns>
-     private IEnumerator SpawnEnemy()
+    private IEnumerator SpawnEnemy()
      {
         yield return new WaitUntil(() => canSpawn);
         EnemySpawn();
         yield return new WaitWhile(() => enemyExsists);
         currentEnemy++;
      }
+    /// <summary>
+    /// Spawns an enemy and adds them to the array of enemies exclusive to this spawnpoint 
+    /// </summary>
     void EnemySpawn()
     {
         Debug.Log($"Spawning {enemyQueue[currentEnemy].name}");
        for(int i = 0; i< spawnsPerEnemy; i++)
         {
-            Instantiate(enemyQueue[currentEnemy], transform.position, transform.rotation);
+            instObj.SetValue(Instantiate(enemyQueue[currentEnemy], transform.position, transform.rotation),currentEnemy);
+            instObj[currentEnemy].name += name;
         }
     }
    
+    private bool CheckForEnemy()
+    {
+        for(int enemies = 0; enemies < instObj.Length; enemies++)
+        {
+            if (instObj[enemies] != null) return true;
+        }
+        return false; 
+    }
     public void StartSpawnRoutine()
     {
         StartCoroutine(SpawnEnemy());
         return; 
     }
+    /// <summary>
+    /// Checks if there are currently any enemies exclusive to this gameobject in the scene
+    /// </summary>
+    /// <returns></returns>
     public bool CanSpawn()
     {
         return canSpawn; 
@@ -49,6 +72,6 @@ public class Spawn : MonoBehaviour
         if (!enemyExsists) canSpawn = designatedTrigger.getActivated();
         else canSpawn = false;
         if (currentEnemy >= enemyQueue.Length) canSpawn = false;
-        enemyExsists = GameObject.FindGameObjectWithTag("Enemy") != null;
+        enemyExsists = CheckForEnemy(); 
     }
 }
