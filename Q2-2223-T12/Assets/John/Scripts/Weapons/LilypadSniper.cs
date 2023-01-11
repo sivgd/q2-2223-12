3d783cd9244d434a34d8fb23d19cd7577d62915b
VGD_2022x23_John_Fotheringham <1713323@mukilteo.wednet.edu>
2023-01-11 08:57:59 -0800

    rl;akfl.esjgl.jgh,sdgjlikgn;sldglkdlsl

--- Q2-2223-T12/Assets/John/Scripts/Weapons/LilypadSniper.cs
+++ Q2-2223-T12/Assets/John/Scripts/Weapons/LilypadSniper.cs
@@ -8,7 +8,6 @@ public class LilypadSniper : MonoBehaviour
     public float recoilAmt = 10f;
     public float animTime = 3f; 
     [Header("External References")]
-    public Vector3 idleOffset = new Vector3(0.55f, 0.28f, 3.25f);
     public GameObject lilypadPrefab;
     public Transform instPos;
     public CameraEffectManager sfx;
@@ -31,19 +30,22 @@ public class LilypadSniper : MonoBehaviour
 
     private void Shoot()
     {
-        //sfx.ApplyRecoil(recoilAmt,transform,initialPos);
-        animator.SetTrigger("IsLilyFling");
-        isAnimating = true;
-       
-        Instantiate(lilypadPrefab, instPos.position, instPos.rotation);
-       //transform.localPosition = transform.localPosition;     
+        if (!isAnimating)
+        {
+            //sfx.ApplyRecoil(recoilAmt,transform,initialPos);
+            animator.SetTrigger("IsLilyFling");
+            isAnimating = true;
+            StartCoroutine(WaitForAnimationDone()); 
+            Instantiate(lilypadPrefab, instPos.position, instPos.rotation);
+            //transform.localPosition = transform.localPosition;     
+        }
     }
-   /* IEnumerator WaitForAnimationDone()
+    IEnumerator WaitForAnimationDone()
     {
         yield return new WaitForSecondsRealtime(animTime - 0.01f);
         isAnimating = false;
-        transform.localPosition = idleOffset; 
-    }*/
+    
+    }
     public bool GetAnimating()
     {
         return isAnimating; 
