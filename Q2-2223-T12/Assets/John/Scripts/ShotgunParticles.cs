c2be8d15d0d34929293067543d9f047cc9074493
VGD_2022x23_John_Fotheringham <1713323@mukilteo.wednet.edu>
2023-01-24 08:41:40 -0800

    this should fix it

--- Q2-2223-T12/Assets/John/Scripts/ShotgunParticles.cs
+++ Q2-2223-T12/Assets/John/Scripts/ShotgunParticles.cs
@@ -8,6 +8,7 @@ public class ShotgunParticles : MonoBehaviour
     public float spread = 0.1f; 
     public float pelletSpeed = 0.3f;
     private bool allowForPelletTravel;
+    private Vector3 spreadVector; 
     private float pointInTravel = 0f; 
     
     private void Awake()
@@ -16,10 +17,9 @@ public class ShotgunParticles : MonoBehaviour
         dX = Random.Range(-spread, spread);
         dY = Random.Range(-spread, spread);
         dZ = Random.Range(-spread, spread);
-        Vector3 spreadVector = new Vector3(dX, dY, dZ);
-        Vector3 v = transform.position + (transform.up+ spreadVector); 
+        spreadVector = new Vector3(0f, dY, dZ); 
         allowForPelletTravel = true;
-        transform.rotation = Quaternion.LookRotation(v); 
+        transform.rotation = Quaternion.LookRotation(transform.up.normalized + spreadVector); 
     }
 
     private void Update()
@@ -32,7 +32,7 @@ public class ShotgunParticles : MonoBehaviour
     }
     void pelletTravel()
     {
-        transform.position = Vector3.Lerp(transform.position, transform.position + (-transform.up * rangeOfTravel), pointInTravel);
-        if (transform.position.Equals(transform.position + (-transform.up * rangeOfTravel))) Destroy(gameObject); 
+        transform.localPosition = Vector3.Lerp(transform.localPosition, transform.localPosition + (transform.up.normalized * rangeOfTravel),pointInTravel );
+        if (transform.position.Equals(transform.position + (transform.up * rangeOfTravel))) Destroy(gameObject); 
     }
 }
