4113b312d275602384160766c8dbb713b10ca71b
logancox987 <logancox987@gmail.com>
2023-02-01 16:04:23 -0800

    end cutscene

---
+++ Q2-2223-T12/Assets/Logan/UIAssets/panelMoveEnd.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class panelMoveEnd : MonoBehaviour
+{
+    public GameObject[] comicPanels;
+    public int currentPanel;
+    public int lastPanel;
+
+    private void Update()
+    {
+        for (int i = 0; i < comicPanels.Length; i++)
+        {
+            lastPanel = i;
+        }
+
+    }
+
+    public void mouseClick()
+    {
+        if (comicPanels[currentPanel] != comicPanels[10])
+        {
+            Debug.Log("a"); ;
+            comicPanels[currentPanel].SetActive(false);
+            currentPanel++;
+        }
+        else if (comicPanels[currentPanel] == comicPanels[10])
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
+    }
+
+}
